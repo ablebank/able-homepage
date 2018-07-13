c0599fa4050c3257a27bcfb5f9bdaeb31612afec
chise44 <chise4455@gmail.com>
2018-07-13 10:26:22 +0900

    1

---
+++ index1.php
@@ -0,0 +1,294 @@
+<!DOCTYPE html>
+<html lang="en" />
+
+<head>
+
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="robots" content="index,follow">
+    <meta name="naver-site-verification" content="68df9dfa8ffc5b2ecb3e1b9f6c7217186fd9144c"/>
+
+    <meta property="og:title" content="k-blockchains" />
+    <meta property="og:url" content="http://k-blockchains.com" />
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="http://k-blockchains.com/img/meta_logo.png" />
+    <meta property="og:description" content="K-Blockchain은 4차 산업의 주요 분야 중에 하나인 블록체인 선두 기업입니다." />
+
+    <meta name="twitter:title" content="k-blockchains">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:description" content="K-Blockchain은 4차 산업의 주요 분야 중에 하나인 블록체인 선두 기업입니다.">
+    <meta name="twitter:image" content="http://k-blockchains.com/img/meta_logo.png">
+    <meta name="twitter:domain" content="k-blockchains">
+
+    <title>K BLOCK CHAIN</title>
+    <link rel="canonical" href="http://k-blockchains.com/index.html">
+    <meta name="description" content="K-Blockchain은 4차 산업의 주요 분야 중에 하나인 블록체인 선두 기업입니다.">
+    <!-- Bootstrap core CSS -->
+    <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/vendor/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="/vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Noto+Sans' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+
+    <!-- Custom styles for this template -->
+    <link href="/css/main.css?d=20170917" rel="stylesheet" type="text/css" />
+</head>
+
+<body id="page-top">
+
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div class="container-fluid">
+        <a class="navbar-brand js-scroll-trigger" href="/">
+            <img src='/img/logo_white_kbc.png' width="131" height="68.7px"/>
+        </a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fa fa-bars"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="#Home">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="#About">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="#Business">Business</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="#Vision">Vision</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="#Partner">Partner</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link js-scroll-trigger" href="#Contact">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<!-- Header -->
+<header class="masthead mask" id="Home">
+    <div class="container homeContainer">
+        <div class="row mainContent">
+            <div class="first col">
+                <h4 class="title">K-Blockchain</h4>
+                <p class="p1">4차 산업혁명의 핵심 블록체인의 퍼스트 무버, K-Blockchain입니다.</p>
+                <p class="p2">K-Blockchain은 4차 산업혁명의 핵심인 블록체인 선두 기업입니다. 고도화된 핀테크 기술과 마케팅 서비스를 바탕으로 블록체인 기술을 실생활에 접목하고 이를 통해 모든 것을 할 수 있는 세상을 만들기 위해 노력합니다.</p>
+            </div>
+            <div class="second col">
+                <div><img class="rightImg" src="../img/right_img.png"></div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<div class="layer-1">
+    <div class="widebar"></div>
+</div>
+<div class="layer-2">
+    <div class="widebar"></div>
+</div>
+
+<!-- About -->
+<section id="About">
+    <div class="container">
+        <div class="row">
+            <div class="first col">
+                <img src="../img/img_about_01.png">
+            </div>
+            <div class="second col">
+                <h4 class="title">ABOUT US</h4>
+                <p class="p1">현재 블록체인 기술은 꽃을 피우기 위한 기초단계에 있습니다. 블록체인 기술이 적용된 암호화폐는 그 가치를 인정받아 새로운 화폐이자 투자자산으로 주목받고 있습니다.<br><br>하지만 초기단계인 만큼 불완전한 기술개발, 부족한 제도 및 시스템으로 인해 급격한 변화를 보이고 있습니다.<br><br>
+                    이로 인해 암호화폐 시장은 블록체인의 이해와 분석을 통한 투자보다는 투기에 가까운 열풍 속에서 성장해 나가고 있습니다.<br><br>K-Blockchain은 블록체인, 금융, IT 등 각 분야의 전문가들로 이루어진 기업으로 블록체인에 대한 올바른 이해와 암호화폐 시장의 건전한 성장을 이끌고자 합니다.<br><br>
+                    블록체인 기술을 기반으로 한  암호화폐를 기반으로 블록체인 기술개발 및 인재양성을 목표로 합니다. 블록체인 스타트업 발굴 및 투자, 글로벌 마케팅 지원 등의 다양한 활동을 통해 블록체인과 암호화폐 시장의 발전과 성장을 위해 끊임없는 노력을 할 것입니다.
+                </p>
+            </div>
+            <div class="mobile-view">
+                <img src="../img/img_about_01.png">
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="layer-3">
+    <div class="widebar"></div>
+</div>
+<div class="layer-4">
+    <div class="widebar"></div>
+</div>
+
+<!-- About -->
+<section id="Business">
+    <div class="container">
+        <h3 class="title">BUSINESS</h3>
+        <p class="p1">K-BlockChain은 블록체인 기술을 바탕으로 한 암호화폐 개발에서부터 인재 양성, 블록체인 스타트업 엑셀러이팅까지 블록체인 산업 전반에 걸친 성장을 위해 노력합니다.</p>
+
+        <div class="box">
+            <h4 class="box_title">01. 블록체인 인재양성</h4>
+            <p class="box_p1">블록체인 스타트업 발굴 및 기술 논의를 위한 컨퍼런스 등 다양한 서비스를 제공합니다.</p>
+        </div>
+
+        <div class="row listBox">
+            <div>
+                <h4>엑셀러레이팅</h4>
+                <p>유망 블록체인 스타트업 발굴,투자</p>
+            </div>
+            <div>
+                <h4>블록체인<br>교육</h4>
+                <p>블록체인 컨퍼런스 개최</p>
+            </div>
+            <div>
+                <h4>블록체인<br>개발자 양성</h4>
+                <p>블록체인 프로젝트를 통한 개발자 양성</p>
+            </div>
+            <div>
+                <h4>블록체인<br>마케팅 대행</h4>
+                <p>국내외, 블록체인 마케팅 대행 및 네트워킹 형성</p>
+            </div>
+        </div>
+
+
+        <div class="box">
+            <h4 class="box_title">02. 블록체인 관련 투자, 개발</h4>
+            <p class="box_p1">분야별 블록체인 프로젝트를 통해 블록체인 성장 다각화를 추구합니다. </p>
+        </div>
+
+        <div class="longbox-display-1920">
+            <div class="row longbox_1">
+                <div class="longbox_cir longbox_cir_start"><span class="longbox_cir_1 blueTxt" style="padding-top:35px;">금융<br>플랫폼</span></div>
+                <div class="longbox_cir"><span class="longbox_cir_2" style="padding-top:35px;">P2P 대출<br> 매칭</span></div>
+                <div class="longbox_cir"><span class="longbox_cir_3" style="padding-top:20px;">암호화폐 <br>기반 <br>신용시스템</span></div>
+                <div class="longbox_cir"><span class="longbox_cir_4" style="padding-top:35px;">탈중앙화 <br>거래소</span></div>
+                <div class="longbox_cir longbox_cir_end"><span class="longbox_cir_5" style="padding-top:55px;">페이먼트</span></div>
+            </div>
+
+            <div class="row longbox_2">
+                <div class="longbox_cir longbox_cir_start"><span class="longbox_cir_1 blueTxt" style="padding-top:55px;">4차 산업</span></div>
+                <div class="longbox_cir"><span class="longbox_cir_2" style="padding-top:55px;">바이오</span></div>
+                <div class="longbox_cir"><span class="longbox_cir_3" style="padding-top:55px;">IOT</span></div>
+                <div class="longbox_cir longbox_cir_end"><span class="longbox_cir_5" style="padding-top:55px;">AI</span></div>
+            </div>
+            <div class="row longbox_2">
+                <div class="longbox_cir longbox_cir_start"><span class="longbox_cir_1 blueTxt" style="padding-top:55px;">기타</span></div>
+                <div class="longbox_cir"><span class="longbox_cir_2" style="padding-top:55px;">부동산</span></div>
+                <div class="longbox_cir"><span class="longbox_cir_3" style="padding-top:55px;">교육</span></div>
+                <div class="longbox_cir longbox_cir_end"><span class="longbox_cir_5" style="padding-top:55px;">전자상거래</span></div>
+            </div>
+        </div>
+        <div class="longbox-display-mobile">
+            <div class="row">
+                <div class="longbox-mobile">
+                    <div class="longbox_cir longbox_cir_start" style="padding-top:12px;"><span class="longbox_cir_1 blueTxt">금융<br>플랫폼</span></div>
+                    <div class="longbox_cir longbox_cir_middle" style="padding-top:14px;"><span class="longbox_cir_2" style="padding-top:35px;">P2P 대출<br> 매칭</span></div>
+                    <div class="longbox_cir longbox_cir_middle" style="padding-top:14px;"><span class="longbox_cir_3" style="padding-top:20px;">암호화폐 <br>신용시스템</span></div>
+                    <div class="longbox_cir longbox_cir_middle" style="padding-top:14px;"><span class="longbox_cir_4" style="padding-top:35px;">탈중앙화 <br>거래소</span></div>
+                    <div class="longbox_cir longbox_cir_end" style="padding-top:22px;"><span class="longbox_cir_5" style="padding-top:55px;">페이먼트</span></div>
+                </div>
+                <div class="longbox-mobile">
+                    <div class="longbox_cir longbox_cir_start" style="padding-top:22px;"><span class="longbox_cir_1 blueTxt">4차 산업</span></div>
+                    <div class="longbox_cir longbox_cir_middle_v2" style="padding-top:22px;"><span class="longbox_cir_2">바이오</span></div>
+                    <div class="longbox_cir longbox_cir_middle_v2" style="padding-top:22px;"><span class="longbox_cir_3">IOT</span></div>
+                    <div class="longbox_cir longbox_cir_end_v2" style="padding-top:22px;"><span class="longbox_cir_4">AI</span></div>
+                </div>
+                <div class="longbox-mobile">
+                    <div class="longbox_cir longbox_cir_start" style="padding-top:22px;"><span class="longbox_cir_1 blueTxt">기타</span></div>
+                    <div class="longbox_cir longbox_cir_middle_v2" style="padding-top:22px;"><span class="longbox_cir_2">부동산</span></div>
+                    <div class="longbox_cir longbox_cir_middle_v2" style="padding-top:22px;"><span class="longbox_cir_3">교육</span></div>
+                    <div class="longbox_cir longbox_cir_end_v2" style="padding-top:22px;"><span class="longbox_cir_4">전자상거래</span></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--vision-->
+<section id="Vision">
+    <div class="container">
+        <div class="row mainBox">
+            <div class="col leftBox">
+                <h3 class="title">VISION</h3>
+                <p class="p1">블록체인을 통해 하나로 연결된 세상을 만듭니다.</p>
+                <p class="p2">현재 블록체인은 인터넷이 처음 보급되었던 시기와 비교되고 있습니다. 인터넷의 발달로 메스미디어에서 일방적으로 정보를 전달하던 시대에서 사람들이 의견을 주고받으며, 정보를 공유하는 시대로 탈바꿈하면서 전 세계가 하나로 연결되었습니다.<br><br>
+                    블록체인은 거래이력이 기록된 일종의 장부로, 기록 및 검증 과정에 모든 사용자가 참여합니다. 위 변조할 수 없는 뿐만 아니라 사용자가 늘수록 안전성은 더욱 강화됩니다.<br><br>
+                    하나의 주체가 정보를 컨트롤 하는 것이 아니라 사용자로 인해 만들어지고 발전해나가는 세상을 만드는 것입니다.<br><br>
+                    K-Blockchain이 꿈꾸는 세상은 K-Blockchain이 만들고 있는 세상과 같습니다. 블록체인이 국경을 넘어 사용자와 사용자를 연결하듯 블록체인으로 세상을 연결하고 자산운용부터 투자, 무역까지 모든것을 할 수 있는 새로운 세상을 만들기 위해 노력합니다.
+                </p>
+            </div>
+            <div class="col rightBox">
+                <h3 class="a1">All</h3>
+                <h3 class="a2">블록체인을 통한 자산운용, 개발과 투자를 통해 최고의 금융서비스와 새로운 가치를 만들어 냅니다.<br> 우리 생활에 밀접한 서비스 창출을 통해 블록체인으로 모든 것을 할 수 있으며,<br> 생활 편의를 만드는 데 기여하고자노력합니다.</h3>
+                <div class="row innerBox">
+                    <div class="col">
+                        <h4>Infinity</h4>
+                        <p>자산 운용과 금융 서비스를 바탕으로 생활 밀착 서비스에서부터 새로운 가치 창출까지 산업 전반으로 무한한 확장이 가능합니다.</p>
+                    </div>
+                    <div class="col">
+                        <h4>Conneetion</h4>
+                        <p>사용자의 사용자를 연결하는 블록체인 기술을 통해 세상을 연결하고 사용자들이 올바른 정보를 공유 하고, 소통하는 문화를 만듭니다.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="layer-5">
+    <div class="widebar"></div>
+</div>
+
+<!-- partner -->
+<section id="Partner">
+    <div class="container">
+        <h3 class="title">PARTNER</h3>
+        <p class="p1">K-BlockChain의 파트너사를 소개합니다!</p>
+        <div class="row partnerRow">
+            <div class="col"><img src="/img/partner_01.png"></div>
+            <div class="col"><img src="/img/partner_02.png"></div>
+        </div>
+    </div>
+</section>
+
+<!-- partner -->
+<section id="Contact">
+    <div class="container">
+        <h3 class="title">CONTACT US</h3>
+        <p class="p1">CEO : Kim, Myeong so</p>
+        <p class="p1">Business Registration Number: 462-86-00783</p>
+        <p class="p1">Copyright © 2017 k-blockchain Inc. All rights reserved</p>
+
+        <div class="sns_list">
+            <a href="mailto:kblockchain@gmail.com"><img src="../img/btn_mail.png"></a>
+            <a href="https://www.facebook.com/Kblockchains/"><img src="../img/btn_facebook.png"></a>
+            <a href="https://twitter.com/kblockchains"><img src="../img/btn_twitter.png"></a>
+        </div>
+    </div>
+</section>
+
+<!-- Bootstrap core JavaScript -->
+<script src="/vendor/jquery/jquery.min.js"></script>
+<script src="/vendor/popper/popper.min.js"></script>
+<script src="/vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+<!-- Contact form JavaScript -->
+<!--<script src="/js/jqBootstrapValidation.js"></script>
+<script src="/js/contact_me.js"></script>-->
+
+<!-- Custom scripts for this template -->
+<script src="/js/agency.min.js"></script>
+
+</body>
+
+</html>
